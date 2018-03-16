17ada96ea70e842e8b46c5918760ea48c540eb80
ckhoarmnp <sorapol57@gmail.com>
2018-03-17 05:43:56 +0700

    Added : Basic enemy

--- LifeIsArt/Assets/Script/BaseEnemy.cs
+++ LifeIsArt/Assets/Script/BaseEnemy.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class BaseEnemy : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+  [SerializeField]
+  protected float _Speed;
+
+  private GameObject _Target;
+  private Vector3 _PlayerDirection;
+
+  void Awake()
+  {
+    _Target = GameObject.FindGameObjectWithTag("Player");
+    Vector3 player = _Target.transform.position;
+    Vector3 direction = new Vector3(transform.position.x - _Target.transform.position.x, transform.position.y - _Target.transform.position.y);
+    transform.up = direction;
+    _PlayerDirection = Vector3.Normalize(direction);
+  }
+
+  void FixedUpdate()
+  {
+    OnMove();
+  }
+
+  protected virtual void OnMove()
+  {
+    MovingToPlayerPosition();
+  }
+
+  private void MovingToPlayerPosition()
+  {
+    transform.position = new Vector3(transform.position.x - (_PlayerDirection.x * _Speed), transform.position.y - (_PlayerDirection.y * _Speed), 0.0f);
+  }
 }
--- LifeIsArt/Assets/Script/Enemy.cs
+++ LifeIsArt/Assets/Script/Enemy.cs
@@ -2,15 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : BaseEnemy {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+  void OnMove()
+  {
+
+  }
 }
