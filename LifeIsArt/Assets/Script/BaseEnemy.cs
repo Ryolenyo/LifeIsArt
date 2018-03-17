66c4f02446038e99d0b61bfb69a1d35f23424901
Ryolenyo <ryolenyo@gmail.com>
2018-03-17 19:47:36 +0700

    sin wave

--- LifeIsArt/Assets/Script/BaseEnemy.cs
+++ LifeIsArt/Assets/Script/BaseEnemy.cs
@@ -13,14 +13,29 @@ public class BaseEnemy : MonoBehaviour {
   private GameObject _Target;
   private Vector3 _PlayerDirection;
 
-  void Awake()
+  public float min;
+  public float max;
+
+  public  float firstpox;
+  public  float firstpoy;
+    public int dir;
+
+
+
+    void Awake()
   {
     _Target = GameObject.FindGameObjectWithTag("Player");
     Vector3 player = _Target.transform.position;
     Vector3 direction = new Vector3(transform.position.x - _Target.transform.position.x, transform.position.y - _Target.transform.position.y);
     transform.up = direction;
     _PlayerDirection = Vector3.Normalize(direction);
-  }
+    firstpox = transform.position.x;
+    firstpoy = transform.position.y;
+
+    System.Random rand = new System.Random();
+    dir = rand.Next(0, 4);
+
+    }
 
   void FixedUpdate()
   {
@@ -35,13 +50,69 @@ public class BaseEnemy : MonoBehaviour {
 
   protected virtual void OnMove()
   {
-    MovingToPlayerPosition();
+        if (transform.tag == "E1")
+        {
+            sinMove();
+        }
+        else if (transform.tag == "E2")
+        {
+            MovingToPlayerPosition();
+        }
+        else if (transform.tag == "E4")
+        {
+            MovingToPlayerPosition();
+            spinMove();
+        }
   }
 
   protected virtual void OnScaling()
   {
     ScaleEnemy();
   }
+
+    private float choose = 0.5f;
+
+    private void spinMove()
+    {
+        transform.Rotate(0,0,90);
+    }
+    private void sinMove()
+  {
+        float pox;
+        float poy;
+        if ((firstpoy + min > transform.position.y || firstpoy + max < transform.position.y) && (dir == 0 || dir == 1))
+        {
+            choose = choose * -1;
+        }
+        else if ((firstpox + min > transform.position.x || firstpox + max < transform.position.x) && (dir == 2 || dir == 3))
+        {
+            choose = choose * -1;
+        }
+
+        if (dir == 0)
+        {
+            pox = transform.position.x + 0.5f;
+            poy = transform.position.y + choose;
+        }
+        else if (dir == 1)
+        {
+            pox = transform.position.x - 0.5f;
+            poy = transform.position.y + choose;
+        }
+        else if (dir == 2)
+        {
+            pox = transform.position.x + choose;
+            poy = transform.position.y + 0.5f;
+        }
+        else
+        {
+            pox = transform.position.x + choose;
+            poy = transform.position.y - 0.5f;
+        }
+        
+
+        transform.position = new Vector3(pox, poy, 0.0f);
+    }
 
   private void MovingToPlayerPosition()
   {
--- LifeIsArt/Assets/Script/EnemySpawnPointController.cs
+++ LifeIsArt/Assets/Script/EnemySpawnPointController.cs
@@ -2,103 +2,108 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawnPointController : MonoBehaviour {
+public class EnemySpawnPointController : MonoBehaviour
+{
 
-  [SerializeField]
-  private float _SetNewValueTime;
-  [SerializeField]
-  private GameObject _EnemyPrefab;
+    [SerializeField]
+    private float _SetNewValueTime;
+    [SerializeField]
+    private GameObject[] enemylist;
 
-  //This variable is up to position in hierachy of spawn point.
-  private float _MaxRandomRangeX;
-  private float _MaxRandomRangeY;
-  private int _NumberOfEnemy;
-  private Dictionary<int, float> _CooldownPerGate;
-  private Dictionary<int, Vector3> _SpawnPoints;
-  private float _Time;
-  private Dictionary<int, float> _CountGateCooldown;
+    //This variable is up to position in hierachy of spawn point.
+    private float _MaxRandomRangeX;
+    private float _MaxRandomRangeY;
+    private int _NumberOfEnemy;
+    private Dictionary<int, float> _CooldownPerGate;
+    private Dictionary<int, Vector3> _SpawnPoints;
+    private float _Time;
+    private Dictionary<int, float> _CountGateCooldown;
 
-  void Awake()
-  {
-    _Time = 0.0f;
-    _MaxRandomRangeY = GameObject.Find("BottomBG").GetComponent<SpriteRenderer>().bounds.size.y;
-    _MaxRandomRangeX = GameObject.Find("BottomBG").GetComponent<SpriteRenderer>().bounds.size.x;
-  }
-
-  void Start()
-  {
-    SetNewValue();
-    SpawnEnemy();
-  }
-
-  void FixedUpdate()
-  {
-    SpawnEnemy();
-
-    _Time += Time.deltaTime;
-    if (_Time > _SetNewValueTime)
+    void Awake()
     {
-      SetNewValue();
-      _Time = 0;
+        _Time = 0.0f;
+        _MaxRandomRangeY = GameObject.Find("BottomBG").GetComponent<SpriteRenderer>().bounds.size.y;
+        _MaxRandomRangeX = GameObject.Find("BottomBG").GetComponent<SpriteRenderer>().bounds.size.x;
     }
-  }
 
-  private void SpawnEnemy()
-  {
-    foreach(KeyValuePair<int, float> entry in _CooldownPerGate)
+    void Start()
     {
-      _CountGateCooldown[entry.Key] += Time.deltaTime;
-      if (_CountGateCooldown[entry.Key] > entry.Value)
-      {
-        GameObject enemy = Instantiate(_EnemyPrefab, _SpawnPoints[entry.Key], Quaternion.identity);
-        enemy.GetComponent<Enemy>().SetSpeed(Random.Range(0.3f, 1.2f));
-        enemy.GetComponent<Enemy>().SetScale(Random.Range(3.0f, 18.0f)); // making for variant size TODO: have to make big boss size that create at normal distribution.
-        _CountGateCooldown[entry.Key] = 0.0f;
-      }
+        SetNewValue();
+        SpawnEnemy();
     }
-  }
 
-  private void SetNewValue()
-  {
-    _SpawnPoints = new Dictionary<int, Vector3>();
-    _CooldownPerGate = new Dictionary<int, float>();
-    _CountGateCooldown = new Dictionary<int, float>();
-
-    System.Random rng = new System.Random();
-    //random number of enemy.
-    _NumberOfEnemy = rng.Next(3,9);
-
-    int x = 0;
-    int y = 0;
-    for(int i = 0; i < _NumberOfEnemy; i++)
+    void FixedUpdate()
     {
-      // random up down.
-      if (rng.Next(0, 2) == 0)
-      {
-        //up
-        y = rng.Next(Mathf.RoundToInt(_MaxRandomRangeY / 2 ) - 6, Mathf.RoundToInt(_MaxRandomRangeY / 2));
-      }
-      else
-      {
-        //down
-        y = rng.Next(-Mathf.RoundToInt(_MaxRandomRangeY / 2), -Mathf.RoundToInt(_MaxRandomRangeY / 2 ) + 6);
-      }
+        SpawnEnemy();
 
-      // random left right.
-      if (rng.Next(0, 2) == 0)
-      {
-        //left
-        x = rng.Next(-Mathf.RoundToInt(_MaxRandomRangeX / 2), -Mathf.RoundToInt(_MaxRandomRangeX / 2) + 6);
-      }
-      else
-      {
-        //right
-        x = rng.Next(Mathf.RoundToInt(_MaxRandomRangeX / 2) - 6, Mathf.RoundToInt(_MaxRandomRangeX / 2));
-      }
-
-      _SpawnPoints[i] = new Vector3(x, y);
-      _CooldownPerGate[i] = rng.Next(1,3);
-      _CountGateCooldown[i] = 0.0f;
+        _Time += Time.deltaTime;
+        if (_Time > _SetNewValueTime)
+        {
+            SetNewValue();
+            _Time = 0;
+        }
     }
-  }
+
+    private void SpawnEnemy()
+    {
+        foreach (KeyValuePair<int, float> entry in _CooldownPerGate)
+        {
+            _CountGateCooldown[entry.Key] += Time.deltaTime;
+            if (_CountGateCooldown[entry.Key] > entry.Value)
+            {
+
+                Random rnd = new Random();
+                int num = Random.Range(0, enemylist.Length);
+                GameObject _EnemyPrefab = enemylist[num];
+                GameObject enemy = Instantiate(_EnemyPrefab, _SpawnPoints[entry.Key], Quaternion.identity);
+                enemy.GetComponent<Enemy>().SetSpeed(Random.Range(0.3f, 1.2f));
+                enemy.GetComponent<Enemy>().SetScale(Random.Range(0.5f, 2.0f)); // making for variant size TODO: have to make big boss size that create at normal distribution.
+                _CountGateCooldown[entry.Key] = 0.0f;
+            }
+        }
+    }
+
+    private void SetNewValue()
+    {
+        _SpawnPoints = new Dictionary<int, Vector3>();
+        _CooldownPerGate = new Dictionary<int, float>();
+        _CountGateCooldown = new Dictionary<int, float>();
+
+        System.Random rng = new System.Random();
+        //random number of enemy.
+        _NumberOfEnemy = rng.Next(3, 9);
+
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < _NumberOfEnemy; i++)
+        {
+            // random up down.
+            if (rng.Next(0, 2) == 0)
+            {
+                //up
+                y = rng.Next(Mathf.RoundToInt(_MaxRandomRangeY / 2) - 6, Mathf.RoundToInt(_MaxRandomRangeY / 2));
+            }
+            else
+            {
+                //down
+                y = rng.Next(-Mathf.RoundToInt(_MaxRandomRangeY / 2), -Mathf.RoundToInt(_MaxRandomRangeY / 2) + 6);
+            }
+
+            // random left right.
+            if (rng.Next(0, 2) == 0)
+            {
+                //left
+                x = rng.Next(-Mathf.RoundToInt(_MaxRandomRangeX / 2), -Mathf.RoundToInt(_MaxRandomRangeX / 2) + 6);
+            }
+            else
+            {
+                //right
+                x = rng.Next(Mathf.RoundToInt(_MaxRandomRangeX / 2) - 6, Mathf.RoundToInt(_MaxRandomRangeX / 2));
+            }
+
+            _SpawnPoints[i] = new Vector3(x, y);
+            _CooldownPerGate[i] = rng.Next(1, 3);
+            _CountGateCooldown[i] = 0.0f;
+        }
+    }
 }
