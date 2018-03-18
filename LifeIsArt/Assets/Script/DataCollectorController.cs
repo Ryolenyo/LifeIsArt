cf5cadc29a2628e4a48c8380232a9cbf2e0a1278
ckhoarmnp <sorapol57@gmail.com>
2018-03-18 08:51:56 +0700

    mock up datacollector and change shadow alpha

---
+++ LifeIsArt/Assets/Script/DataCollectorController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataCollectorController : MonoBehaviour {
+
+  public void AddStat(string action)
+  {
+    PlayerPrefs.SetInt(action, PlayerPrefs.GetInt(action, 0) + 1);
+  }
+}
