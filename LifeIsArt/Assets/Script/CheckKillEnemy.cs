975ac36b54a6b4b0728deb5073bd253c860afcc9
ckhoarmnp <sorapol57@gmail.com>
2018-03-18 14:04:02 +0700

    revive slam

---
+++ LifeIsArt/Assets/Script/CheckKillEnemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckKillEnemy : MonoBehaviour {
+
+  void OnTriggerEnter(Collider other)
+  {
+    if (other.gameObject.CompareTag("E1") || other.gameObject.CompareTag("E2") || other.gameObject.CompareTag("E3") || other.gameObject.CompareTag("E4"))
+    {
+      Destroy(other.gameObject);
+    }
+  }
+}
