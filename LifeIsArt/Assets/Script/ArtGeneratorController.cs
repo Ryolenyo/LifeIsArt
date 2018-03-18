ad4e2fbaab644a00862e4e89559e2473914c14ff
ckhoarmnp <sorapol57@gmail.com>
2018-03-18 10:37:03 +0700

    meta

---
+++ LifeIsArt/Assets/Script/ArtGeneratorController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArtGeneratorController : MonoBehaviour {
+
+  [SerializeField]
+  private GameObject[] _BrushType1;
+  [SerializeField]
+  private GameObject[] _BrushType2;
+  [SerializeField]
+  private GameObject[] _BrushType3;
+  [SerializeField]
+  private GameObject[] _BrushType4;
+
+  private float _CanvasWidth;
+  private float _CanvasHeight;
+
+  void Awake()
+  {
+    _CanvasWidth = GetComponent<RectTransform>().rect.width;
+    _CanvasHeight = GetComponent<RectTransform>().rect.height;
+  }
+
+  private void GenerateArt()
+  {
+    int size = Random.Range(5, 15);
+    for (int i = 0; i < size; i++)
+    {
+      //choose brush type
+      int type = Random.Range(1,5);
+      //random brush.color
+      int color = Random.Range(1, 5);
+      //random place
+    }
+  }
+}
--- LifeIsArt/Assets/Script/DestroyEnemy.cs
+++ LifeIsArt/Assets/Script/DestroyEnemy.cs
@@ -2,6 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyEnemy : PlayerController {
+public class DestroyEnemy : MonoBehaviour {
+
+  void OnTriggerEnter(Collider other)
+  {
+    if (other.gameObject.CompareTag("E1") || other.gameObject.CompareTag("E2") || other.gameObject.CompareTag("E3") || other.gameObject.CompareTag("E4"))
+    {
+      Destroy(other.gameObject);
+    }
+  }
 
 }
