17f5fd546f4cc81de5cb92a8f4caabfcbbbb36af
Ryolenyo <ryolenyo@gmail.com>
2018-03-18 14:36:18 +0700

    quit

---
+++ LifeIsArt/Assets/Script/Quit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Quit : MonoBehaviour {
+    public Button quitButton;
+    // Use this for initialization
+    void Start () {
+        quitButton.onClick.AddListener(TaskOnClick);
+       
+    }
+	
+	// Update is called once per frame
+	void TaskOnClick() {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
